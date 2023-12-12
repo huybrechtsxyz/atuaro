36ea922ed3b317ba0e2ded8f97e913183928a28f
Vincent Huybrechts <vhuybrechts@omp.com>
2023-12-12 15:18:21 +0100

    Split downtime per player (remove extra adventure)

--- sources/atuaro-dnddave2023-level2/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Atuaro: D&Dave 2023 Campaign: To level 2
-
-## 1.0
-The first version of the adventure.
--- sources/atuaro-dnddave2023-level2/upgrade.ps1
+++
@@ -1,3 +0,0 @@
-. ./sources/upgrade.ps1
-
-Update-FoundryModule -WorkspacePath "./adventures/dndave2023level2" -SourcePath "./sources/atuaro-dnddave2023-level2"
